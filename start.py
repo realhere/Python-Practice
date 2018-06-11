e1f2c429dba1d1c7a933721896cb6dbc7124066a
Jenson Lee <realhere@gmail.com>
2018-06-11 22:12:22 +0800

    Add files via upload

---
+++ start.py
@@ -0,0 +1,34 @@
+# 資料
+# 數字
+# 3
+# 3.5
+# # print(資料)
+# print(3.5)
+# # 字串
+# "任意的文字"
+# print("什麼文字都可以寫")
+# # 布林值
+# True
+# False
+# print(True)
+# # 變數
+# input=3 # 建立新的變數，把資料 3 放進去
+# print(input)
+# input="Hello World" # 把資料 "Hello World" 覆蓋變數中原來的資料
+# print(input)
+# 運算符號
+# data=5//2 # 整數除法
+# print(data)
+# data=6**3 # 6 的 3 次方
+# print(data)
+# print("Hello"+"World")
+# input(提示文字) 以【字串的形態】取得使用者輸入
+n1=input("Enter a Number:")
+n2=input("Enter a Number:")
+n1=int(n1) # int(資料) 將資料轉換成整數型態
+n2=int(n2)
+result=n1+n2
+print(result)
+# 讓使用者輸入 5 個數字，1. 算總和 2. 哪個數字最大
+# 使用 List 列表處理
+# 回家看老師的網站教學影片用 1.5 倍快轉
