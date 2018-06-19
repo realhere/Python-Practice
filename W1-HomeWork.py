63b26ea53b3154b184198422341cd9bc937a1bc5
Jenson Lee <realhere@gmail.com>
2018-06-19 20:13:11 +0800

    Add files via upload

---
+++ W1-HomeWork.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter a Number:"))
+n2=int(input("Enter a Number:"))
+n3=int(input("Enter a Number:"))
+n4=int(input("Enter a Number:"))
+n5=int(input("Enter a Number:"))
+list1=[n1,n2,n3,n4,n5]
+data=n1+n2+n3+n4+n5
+print("總和：",data) 
+print("最大值：",max(list1))
